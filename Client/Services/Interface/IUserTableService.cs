6018b9c49b82b19d32be97679790c4f83d270bf3
HamzJ18 <131898948+HamzJ18@users.noreply.github.com>
2023-04-26 22:41:48 +0530

    Add files via upload

---
+++ Client/Services/Interface/IUserTableService.cs
@@ -0,0 +1,13 @@
+﻿using MudCRUDlist.Shared.Models;
+
+namespace MudCRUDlist.Client.Services.Interface
+{
+    public interface IUserTableService
+    {
+        Task<IEnumerable<UserTable>> GetUsers();
+        Task<UserTable> GetUser(string UserId);
+        Task<UserTable> AddUser(UserTable entity);
+        Task<UserTable> UpdateUser(string UserId, UserTable entity);
+        Task<UserTable> DeleteUser(string UserID);
+    }
+}
